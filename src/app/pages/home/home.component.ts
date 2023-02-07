f2d0da2038d36ae1fcd3efabf34c393006fab8a3
Rubén González Martín <ruben.gonzalez@afterbanks.com>
2023-02-07 12:08:29 +0100

    google fonts to home page and typedjs effect added

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import Typed from 'typed.js';
 declare var particlesJS: any;
 
 @Component({
@@ -11,6 +11,16 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     particlesJS.load('particles-js', '../../../assets/particles/particles.json', null);
+    const options = {
+      strings: ['Be code my friend...'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      showCursor: true,
+      cursorChar: '|',
+      loop: false
+    };
+
+    const typed = new Typed('#typed-text', options);
   }
 
 }
